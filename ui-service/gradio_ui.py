4afdba103ed84e46db9dd204d8481c3d1a6758a8
AtomicBim <romangman90@gmail.com>
2025-10-06 11:34:24 +0500

    +

--- ui-service/gradio_ui.py
+++ ui-service/gradio_ui.py
@@ -488,7 +488,5 @@ if __name__ == "__main__":
     interface.launch(
         server_name="0.0.0.0",
         server_port=7861,
-        share=False,
-        show_error=True,
-        quiet=False
+        share=False
     )
