87bfd67b57d7baa8b0bdb9bf54cb4d96d14a9d21
AtomicBim <romangman90@gmail.com>
2025-10-06 11:31:40 +0500

    Обновил reamde

--- ui-service/gradio_ui.py
+++ ui-service/gradio_ui.py
@@ -488,5 +488,7 @@ if __name__ == "__main__":
     interface.launch(
         server_name="0.0.0.0",
         server_port=7861,
-        share=False
+        share=False,
+        show_error=True,
+        quiet=False
     )
