e81f1450100c7fcc169d6177f26afb1d7eb72771
AtomicBim <romangman90@gmail.com>
2025-10-10 09:22:20 +0500

    +

--- ui-react-service/src/components/PdfViewer.tsx
+++ ui-react-service/src/components/PdfViewer.tsx
@@ -5,7 +5,7 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import './PdfViewer.css';
 
-// Worker for react-pdf (используем локальный worker из node_modules)
+// Worker for react-pdf - используем локальный worker
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
   import.meta.url,
